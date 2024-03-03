c4d867885e36f9938049168b47714c8c9b87bcf8
Ved <contact.vedant18@gmail.com>
2024-03-03 23:52:39 +0530

    leetcode

--- DP/55. Jump Game.cpp
+++ DP/55. Jump Game.cpp
@@ -8,19 +8,15 @@ public:
     bool canJump(vector<int>& nums) {
         int maxReach = 0;  // Variable to store the maximum reachable index
         int n = nums.size();  // Number of elements in the array
-
         for (int i = 0; i < n; i++) {
             if (i > maxReach) {
                 return false;  // If the current index cannot be reached, return false
             }
-
             maxReach = max(maxReach, i + nums[i]);  // Update the maximum reachable index
-
             if (maxReach >= n - 1) {
                 return true;  // If the last index can be reached, return true
             }
         }
-
         return false;  // If the last index cannot be reached, return false
     }
 };
